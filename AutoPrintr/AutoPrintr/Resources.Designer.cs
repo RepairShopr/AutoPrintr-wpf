f9f183c737b2e0b45909f2a0ca8ab35e7e562ad6
Vitaliy Geruch <geruch.vitaliy@gmail.com>
2016-10-14 12:14:24 +0300

    About fixes

--- AutoPrintr/AutoPrintr/Resources.Designer.cs
+++ AutoPrintr/AutoPrintr/Resources.Designer.cs
@@ -72,11 +72,9 @@ namespace AutoPrintr {
         /// <summary>
         ///   Looks up a localized string similar to The MIT License (MIT)
         ///
-        ///Copyright (c) 2016 RepairShopr 
-        ///
         ///Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
         ///
-        ///The above copyr [rest of string was truncated]&quot;;.
+        ///The above copyright notice and this permission not [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string License {
             get {
--- AutoPrintr/AutoPrintr/ViewModels/AboutViewModel.cs
+++ AutoPrintr/AutoPrintr/ViewModels/AboutViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using AutoPrintr.Helpers;
 using GalaSoft.MvvmLight.Views;
+using System.Linq;
+using System.Reflection;
 
 namespace AutoPrintr.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
         #region Properties
+        public string Product { get; private set; }
+        public string Version { get; private set; }
+        public string Description { get; private set; }
+        public string Copyright { get; private set; }
         public string Licence { get; private set; }
         public string About { get; private set; }
 
@@ -22,6 +28,12 @@ namespace AutoPrintr.ViewModels
         public override void NavigatedTo(object parameter = null)
         {
             base.NavigatedTo(parameter);
+
+            var assembly = Assembly.GetExecutingAssembly();
+            Product = assembly.GetCustomAttributes(false).OfType<AssemblyProductAttribute>().Select(x => x.Product).First();
+            Version = assembly.GetName().Version.ToString();
+            Description = assembly.GetCustomAttributes(false).OfType<AssemblyDescriptionAttribute>().Select(x => x.Description).First();
+            Copyright = $"{assembly.GetCustomAttributes(false).OfType<AssemblyCopyrightAttribute>().Select(x => x.Copyright).First()} {assembly.GetCustomAttributes(false).OfType<AssemblyCompanyAttribute>().Select(x => x.Company).First()}";
 
             Licence = Resources.License;
             About = Resources.About;
