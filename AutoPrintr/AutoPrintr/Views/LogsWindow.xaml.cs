c8f20aeba9411a0bf4324403d3b6131b6d649d9e
Vitaliy Geruch <geruch.vitaliy@gmail.com>
2016-11-01 11:02:30 +0200

    Small DatePicker fix

--- AutoPrintr/AutoPrintr/Views/LogsWindow.xaml.cs
+++ AutoPrintr/AutoPrintr/Views/LogsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AutoPrintr.Views
 {
@@ -7,6 +9,12 @@ namespace AutoPrintr.Views
         public LogsWindow()
         {
             InitializeComponent();
+        }
+
+        private void DatePicker_Loaded(object sender, RoutedEventArgs e)
+        {
+            var datePicker = (DatePicker)sender;
+            datePicker.BlackoutDates.Add(new CalendarDateRange { Start = DateTime.Today.AddDays(1) });
         }
     }
 }
