0c0afba79cfc477777e0f1351fe118f0f302b144
Vitaliy Geruch <geruch.vitaliy@gmail.com>
2016-11-23 11:17:39 +0200

    Added code which checks the required version of .NET Frameword

--- AutoPrintr/AutoPrintr/App.xaml.cs
+++ AutoPrintr/AutoPrintr/App.xaml.cs
@@ -2,6 +2,7 @@
 using AutoPrintr.Helpers;
 using AutoPrintr.Views;
 using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -28,6 +29,22 @@ namespace AutoPrintr
         protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            if (!CheckIsNet45Installed())
+            {
+                var message = "This application require .NET Framework 4.5 or above. Would you like to install the latest version of .NET Framework now?";
+                var caption = ".NET Framework 4.5 is not installed";
+
+                if (MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
+                {
+                    var downloadUrl = "https://www.microsoft.com/en-US/download/details.aspx?id=49981";
+                    Process.Start(downloadUrl);
+                }
+
+                Process.GetCurrentProcess().Kill();
+                return;
+            }
+
             await WpfApp.Instance.Startup(e.Args);
             new TrayIconContextMenuView();
         }
@@ -36,6 +53,17 @@ namespace AutoPrintr
         {
             TrayIconContextMenuView.Close();
             base.OnExit(e);
+        }
+
+        private bool CheckIsNet45Installed()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                var netVersion = (int?)ndpKey?.GetValue("Release");
+                //Version of .NET Framework 4.5 is 378389
+                return netVersion >= 378389;
+            }
         }
         #endregion
 
