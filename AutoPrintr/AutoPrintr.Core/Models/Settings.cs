af22b7d80334ee43e15eaff1280934f7d9bb9a31
Vitaliy Geruch <geruch.vitaliy@gmail.com>
2017-02-13 22:13:45 +0200

    Fixed default port number issue

--- AutoPrintr/AutoPrintr.Core/Models/Settings.cs
+++ AutoPrintr/AutoPrintr.Core/Models/Settings.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AutoPrintr.Core.Models
 {
     public class Settings : BaseModel
     {
+        private const int _defaultPortNumber = 7775;
+        private int _portNumber;
+
         public bool AddedToStartup { get; set; }
         public bool InstalledService { get; set; }
-        public int PortNumber { get; set; }
+        public int PortNumber
+        {
+            get { return Math.Max(_defaultPortNumber, _portNumber); }
+            set { _portNumber = value; }
+        }
         public Channel Channel { get; set; }
         public User User { get; set; }
         public IEnumerable<Printer> Printers { get; set; }
@@ -14,7 +22,7 @@ namespace AutoPrintr.Core.Models
 
         public Settings()
         {
-            PortNumber = 7775;
+            PortNumber = _defaultPortNumber;
             Printers = new List<Printer>();
             Locations = new List<Location>();
         }
