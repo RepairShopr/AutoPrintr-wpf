fd6f98407d616a4c81953f754ea2e44acd57e64a
Mikhail Shcherbakov <yuske.dev@gmail.com>
2018-01-14 21:45:58 +0300

    Fix and improve the installer building.

--- AutoPrintr/AutoPrintr/Properties/AssemblyInfo.cs
+++ AutoPrintr/AutoPrintr/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -10,9 +8,6 @@ using System.Windows;
 [assembly: AssemblyTitle("AutoPrintr")]
 [assembly: AssemblyDescription("Automatically print to the desired printer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RepairShopr")]
-[assembly: AssemblyProduct("AutoPrintr")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,17 +34,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page, 
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.18.0")]
-[assembly: AssemblyFileVersion("2.0.18.0")]
